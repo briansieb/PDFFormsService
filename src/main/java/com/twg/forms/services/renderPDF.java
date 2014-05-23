191d66e035a551e373a0e9710cbb1fef7ab22f04
corycowgill <corycowgill@yahoo.com>
2014-05-23 14:42:02 -0500

    yep

---
+++ src/main/java/com/twg/forms/services/renderPDF.java
@@ -0,0 +1,17 @@
+package com.twg.forms.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/renderPDF")
+@Produces(MediaType.APPLICATION_JSON)
+public class renderPDF 
+{
+	@GET
+    public String get() {
+        return "PDF_CALL";
+    }
+}
